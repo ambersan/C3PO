5bd39a0921ae1dc50bb04a39ad2c8b6c036d2f11
vrfelton <43018775+vrfelton@users.noreply.github.com>
2018-10-24 14:16:31 -0400

    Add files via upload
    Started JUnit testing.

---
+++ TasklistTest.java
@@ -0,0 +1,24 @@
+package c3PO;
+
+import static org.junit.*;
+
+import org.junit.Test;
+
+public class TasklistTest {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToggleQueue() {
+		fail("Not yet implemented");
+	}
+
+}
