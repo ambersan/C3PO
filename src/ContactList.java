fa97a425c1876c34dcf8ba885273c7350af569ec
ambersan <ambersan@mtu.edu>
2018-11-12 14:24:46 -0500

    added cellNum

--- src/ContactList.java
+++ src/ContactList.java
@@ -8,12 +8,14 @@ public class ContactList {
 	public static class Contact{
 		String name = "";
 		String num = "";
+		String cellNum = "";
 		String address = "";
 		String email = "";
-		public Contact(String n, String nu, String add, String e){
+		public Contact(String n, String nu, String cellNu, String add, String e){
 			if(!n.equals(null)) {
 				name = n;
 				num = nu;
+				cellNum = cellNu;
 				address = add;
 				email = e;
 			}
@@ -33,6 +35,9 @@ public class ContactList {
 		public void setNum(String n) {
 			num = n;
 		}
+		public void setCellNum(String n) {
+			cellNum = n;
+		}
 		public void setAdd(String a) {
 			address = a;
 		}
@@ -41,6 +46,9 @@ public class ContactList {
 		}
 		public String getNum() {
 			return num;
+		}
+		public String getCellNum() {
+			return cellNum;
 		}
 		public String getAdd() {
 			return address;
@@ -53,8 +61,8 @@ public class ContactList {
 		return c.get(i);
 	}
 	
-	public void add(String name, String num, String add,  String email) {
-			Contact con = new Contact(name, num, add, email);
+	public void add(String name, String num, String cellNum, String add,  String email) {
+			Contact con = new Contact(name, num, cellNum, add, email);
 			int place = 0;
 			for (int i = 0; i < c.size(); i++) {
 				if(c.get(i).name.compareTo(name) < 0) {
@@ -73,16 +81,17 @@ public class ContactList {
 		c.remove(i);
 	}
 	
-	public void edit(String name, String num, String add, String email, int i) {
+	public void edit(String name, String num, String cellNum, String add, String email, int i) {
 
 		if(name.equals(c.get(i).name)){
 			Contact con = c.get(i);
 			con.setNum(num);
+			con.setCellNum(cellNum);
 			con.setAdd(add);
 			con.setEmail(email);
 		}else{
 			delete(i);
-			add(name, num, add, email);
+			add(name, num, cellNum, add, email);
 		}
 
 
